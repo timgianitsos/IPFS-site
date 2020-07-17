62c0427dfcfa30497275904567f84ba5327496da
Vassi Gianitsos <vgianitsos@yahoo.com>
2020-07-16 23:29:05 -0500

    simple login page

--- web-app/src/Login.js
+++ web-app/src/Login.js
@@ -5,10 +5,19 @@ export default class Login extends Component{
 
     render() {
         return (
-            <div className="Login-main">
-              <h1>WHAT'S UP</h1>
-              <p>Welcome to the LOGIN PAGE :0</p>
-            </div>
+          <div className='Log-in'>
+            <h1>Welcome to Facebook :0</h1>
+            <h2>Type Your Username and Password Here:</h2>
+            <form action="">
+              <div className='form-group'>
+                <input type="text" className='form-control' placeholder='Username'/>
+              </div>
+              <div className='form-group'>
+                <input type="password" className='form-control' placeholder='Password'/>
+              </div>
+              <button type="submit" className='btn btn-primary right-btn'>Login</button>
+            </form>
+          </div>
           );
     }
 
